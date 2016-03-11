615cfab915281106e937c6a79abaa4b36e67bbf6
Andreas Leicher <email@andreasleicher.com>
2016-03-11 09:38:00 +0100

    reindent

--- TestIsolationExercise/test/depot.test.js
+++ TestIsolationExercise/test/depot.test.js
@@ -2,42 +2,42 @@
 
 describe('Depot', function () {
 
-    beforeEach(module('priceCalculator'));
+  beforeEach(module('priceCalculator'));
 
-    var stockIds = {
-        sap: "DE0007164600",
-        oracle: "US68389X1054"
-    };
+  var stockIds = {
+    sap: "DE0007164600",
+    oracle: "US68389X1054"
+  };
 
-    var Depot, DepotItem;
-    beforeEach(inject(function (_Depot_, _DepotItem_) {
-        Depot = _Depot_;
-        DepotItem = _DepotItem_;
-    }));
+  var Depot, DepotItem;
+  beforeEach(inject(function (_Depot_, _DepotItem_) {
+    Depot = _Depot_;
+    DepotItem = _DepotItem_;
+  }));
 
-    it('should calculate the total value 0 when it is empty', function () {
-        // TODO Set up an empty depot
-        var depot = new Depot();
+  it('should calculate the total value 0 when it is empty', function () {
+    // TODO Set up an empty depot
+    var depot = new Depot();
 
-        // TODO expect that getTotalValue returns 0
-        expect(depot.getTotalValue()).toEqual(0);
-    });
+    // TODO expect that getTotalValue returns 0
+    expect(depot.getTotalValue()).toEqual(0);
+  });
 
-    it('should calculate the total value of one depot item', function () {
-        // TODO Set up a depot with a stub
+  it('should calculate the total value of one depot item', function () {
+    // TODO Set up a depot with a stub
 
-        // TODO Add one depot item with count 1 to the depot
+    // TODO Add one depot item with count 1 to the depot
 
-        // TODO execute the test method
-    });
+    // TODO execute the test method
+  });
 
-    it('should calculate the total value of multiple depot items', function () {
-        // TODO Set up mock/stub
+  it('should calculate the total value of multiple depot items', function () {
+    // TODO Set up mock/stub
 
-        // TODO Add depot items to depot
+    // TODO Add depot items to depot
 
-        // TODO Execute the test method
+    // TODO Execute the test method
 
-        // TODO Verify results
-    });
+    // TODO Verify results
+  });
 });
